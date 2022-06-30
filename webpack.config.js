8ac1a0675b085286b5df9f3eddc3ff6b4d691469
Sander Paping <sander.paping@student.nhlstenden.com>
2022-06-30 13:27:02 +0200

    changed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
         extensions: [".tsx", ".ts", ".js"],
     },
     devServer: {
-        host: "0.0.0.0",
+        // host: "0.0.0.0",
         port: 8080, //port that we're using for local host (localhost:8080)
         static: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
         hot: true,
