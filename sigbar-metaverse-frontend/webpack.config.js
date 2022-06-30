1059f1ffd73fd86c5d37fccb3cf0e185e6d95c21
sanderpaping <sander.paping@student.stenden.com>
2022-06-30 11:42:56 +0200

    added base

---
+++ sigbar-metaverse-frontend/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const appDirectory = fs.realpathSync(process.cwd());
+
+module.exports = {
+    entry: path.resolve(appDirectory, "src/App.ts"), //path to the main .ts file
+    output: {
+        filename: "js/bundleName.js", //name for the js file that is created/compiled in memory
+        clean: true,
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    devServer: {
+        host: "0.0.0.0",
+        port: 8080, //port that we're using for local host (localhost:8080)
+        static: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
+        hot: true,
+        devMiddleware: {
+            publicPath: "/",
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: path.resolve(appDirectory, "public/index.html"),
+        })
+    ],
+    mode: "development",
+};
