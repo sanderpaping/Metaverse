f5bd21bee0d9a257dfb73818bd01de15446110a0
Sander Paping <sander.paping@student.nhlstenden.com>
2022-07-01 10:52:32 +0200

    changed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,13 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const appDirectory = fs.realpathSync(process.cwd());
 
 module.exports = {
-    entry: path.resolve(appDirectory, "src/App.ts"), //path to the main .ts file
+    // entry: path.resolve(appDirectory, "src/App.ts"), //path to the main .ts file
+    entry: {
+      app: './src/App.ts',
+    },
     output: {
-        filename: "bundle.js", //name for the js file that is created/compiled in memory
+        filename: '[name].bundle.js', //name for the js file that is created/compiled in memory
+        path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
     resolve: {
@@ -16,7 +20,7 @@ module.exports = {
         host: "0.0.0.0",
         allowedHosts: "all",
         port: 8080, //port that we're using for local host (localhost:8080)
-        static: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
+        static: path.resolve(__dirname, "public"), //tells webpack to serve from the public folder
         hot: true,
         devMiddleware: {
             publicPath: "/",
