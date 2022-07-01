2edb23200a7116a792a4aa4b449b10eb77ade2a3
Sander Paping <sander.paping@student.nhlstenden.com>
2022-07-01 13:52:46 +0200

    changed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     },
     devServer: {
         host: "0.0.0.0",
-        // allowedHosts: "all",
+        allowedHosts: "all",
         port: 8080, //port that we're using for local host (localhost:8080)
         static: path.resolve(__dirname, "public"), //tells webpack to serve from the public folder
         hot: true,
